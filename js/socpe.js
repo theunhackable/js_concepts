5c3c93838c74fe6168751f32dca2af933bb6f475
theunhackable <srirangasaipagala@gmail.com>
2023-10-05 21:51:11 +0530

    added every file

--- js/socpe.js
+++ js/socpe.js
@@ -1,8 +1,17 @@
-var x;
-var_dec();
-function var_dec() {
-    x = 10;
+var x = 100;
+// block starts
+{
+    // this x shadows the global x=100
+    var x = 10;
     console.log('inside function scope', x);
 }
+// block ends
 
 console.log('inside global scope', x)
+
+a = 10
+console.log(a)
+
+var a = 20
+
+console.log(a)
